2f45750a1b34ccee70df254d874b52462642a905
Peter Van <peterrvan@gmail.com>
2021-07-07 20:12:56 -0700

    Added models.js and notes in db.js

--- server/routes/db.js
+++ server/routes/db.js
@@ -8,4 +8,27 @@ router.get('/books', dbController.getBooks, (req, res) => {
     return res.status(200).json(res.locals.myBooks);
   })
 
+  // Login
+  // Verify that username exists and matches password in database
+  // SELECT * FROM users...WHERE username = `${query}`...RETURNING _id
+
+  // Sign up
+  // Verify that username is not in the table
+  // Then you insert new entry into the users table, RETURNING _id
+
+  // getBooks
+  // Front end sends backend _id, backend retrieves all volume_ids associated with that user id
+  // Fetch google api for book information pertaining to those volume ids
+  // Return back an array with objects containing all info for books to be rendered on homepage
+
+  // addBook
+
+  // deleteBook
+
+  // updateBook
+
+  // updateNote
+
+  // updateFavorite
+
   module.exports = router;
